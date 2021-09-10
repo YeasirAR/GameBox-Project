a82c288b8d32a3fbedcf73462bc9f571080eb5a1
yeasirar <yeasirar@gmail.com>
2021-09-11 00:47:36 +0600

    Initial Commit

---
+++ src/GameBox/Main.java
@@ -0,0 +1,23 @@
+package GameBox;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("MainFrame.fxml"));
+        primaryStage.setTitle("GameBox");
+        primaryStage.setScene(new Scene(root, 600, 450));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
