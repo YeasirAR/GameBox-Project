c96785fb03ec020d2935d602bbcd3c003a7aef58
yeasirar <yeasirar@gmail.com>
2021-09-30 03:52:23 +0600

    Add code for server-client connection

--- src/GameBox/MainController.java
+++ src/GameBox/MainController.java
@@ -1,17 +1,84 @@
 package GameBox;
 
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
-import java.io.IOException;
+import java.io.*;
+import java.net.Socket;
 import java.util.Objects;
 
+
+
 public class MainController {
+    @FXML
+    private TextField username;
+    @FXML
+    private PasswordField password;
+    @FXML
+    private Label login_failed;
+    @FXML
+    private FontAwesomeIconView userIcon;
+    @FXML
+    private FontAwesomeIconView keyIcon;
+    @FXML
+    private Button login_click;
+//    @FXML
+//    ImageView image;
+//    Image img = new Image(Objects.requireNonNull(getClass().getResourceAsStream("image/logo.png")));
+
+    BufferedReader reader;
+    BufferedWriter writer;
+    public MainController(){
+//        image.setImage(img);
+        Socket sc = null;
+        try {
+            sc = new Socket("127.0.0.1", 6601);
+            OutputStreamWriter o = new OutputStreamWriter(sc.getOutputStream());
+            writer = new BufferedWriter(o);
+            InputStreamReader isr = new InputStreamReader(sc.getInputStream());
+            reader = new BufferedReader(isr);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void login_click(){
+        try {
+            String UserName = username.getText();
+            String Password = password.getText();
+            if (UserName.length() == 0 || Password.length() == 0){
+                login_failed.setText("Please Enter Valid Info");
+            }
+            else {
+                writer.write(UserName + "\n");
+                writer.write(Password + "\n");
+                writer.flush();
+                userIcon.setVisible(false);
+                keyIcon.setVisible(false);
+                password.setVisible(false);
+                username.setVisible(false);
+                login_click.setVisible(false);
+                login_failed.setVisible(false);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @FXML
     private void tic_tac_toe(ActionEvent e) throws IOException {
--- src/Server/GameBoxServer.java
+++ src/Server/GameBoxServer.java
@@ -1,7 +1,66 @@
 package Server;
 
-public class GameBoxServer {
-    public static void main(String[] args) {
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
 
+public class GameBoxServer{
+    public static void main(String[] args) {
+        try {
+            System.out.println("Server is waiting for client.");
+            ServerSocket serverSocket = new ServerSocket(6601);
+
+            while (true){
+                Socket sc = serverSocket.accept();
+                ServerMain serverMain = new ServerMain(sc);
+                Thread t = new Thread(serverMain);
+                t.start();
+            }
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+}
+class ServerMain implements Runnable{
+    BufferedReader reader;
+    BufferedWriter writer;
+
+    final static ArrayList<Users> user = new ArrayList<>();
+
+    ServerMain(Socket sc){
+        try {
+            OutputStreamWriter o = new OutputStreamWriter(sc.getOutputStream());
+            writer = new BufferedWriter(o);
+            InputStreamReader isr = new InputStreamReader(sc.getInputStream());
+            reader = new BufferedReader(isr);
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                user.add(new Users(reader.readLine(), reader.readLine()));
+                System.out.println(user);
+            } catch (IOException e) {
+                e.printStackTrace();
+                break;
+            }
+        }
+    }
+}
+
+class Users{
+    String username;
+    String password;
+
+    public Users(String username, String password) {
+        this.username = username;
+        this.password = password;
     }
 }
